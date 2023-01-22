d768d96318da64832641a807fceb24d43b819413
ampatte <ampatterson1@hotmail.com>
2023-01-21 21:03:32 -0500

    solved the display issues, added styling, and README

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# TestTime-
+# TestTime
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+  This project is a timed, multiple choice coding quiz.
+
+  ## Table of Contents
+  - [Installation Instructions](#Installation)
+  - [Usage Information](#Usage)
+  - [Contribution Guidelines](#Contribution)
+  - [Test Instructions](#Test)
+  - [Questions](#Questions)
+  - [License](#License)
+  
+  ## Acceptance Criteria
+  * GIVEN I am taking a code quiz
+  * WHEN I click the start button
+  * THEN a timer starts and I am presented with a question
+  * WHEN I answer a question
+  * THEN I am presented with another question
+  * WHEN I answer a question incorrectly
+  * THEN time is subtracted from the clock
+  * WHEN all questions are answered or the timer reaches 0
+  * THEN the game is over
+  * WHEN the game is over
+  * THEN I can save my initials and score
+  ## Installation
+  There are no requirements. Just enter the project url into a web browser address bar and start the quiz.
+
+  ## Usage
+  This was created for a homework challenge. It will be used as a mock interview assessment quiz. The problem solved is, preparing me for a career in software development by building my skills and readiness.
+  ![alt text](Assets/images/2023-01-05 (2).png)
+  
+  ## Contribution
+  I was the only contributor. Contact me by email if you are interested in contributing.
+
+  ## Test
+  This project can be tested by actually taking the quiz and seeing the functionality. 
+
+  ## Questions?
+  - Github link: https://github.com/ampatte
+  - Email link: ampatte717@gmail.com
+
+  ## License
+    This project is covered under the MIT license.(https://choosealicense.com/licenses/mit/)
--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 //start button
 document.getElementById("startbtn").innerText = "Begin Quiz";
-document.getElementById("startbtn").addEventListener("click", StartQuiz);
+document.getElementById("startbtn").addEventListener("click", Quiz);
+document.getElementById("1st").addEventListener("click", function(){
+  Quiz(this.value);});
+document.getElementById("2nd").addEventListener("click", function(){
+  Quiz(this.value);});
+document.getElementById("3rd").addEventListener("click",function(){
+Quiz(this.value);});
+document.addEventListener("click", function(){viewHighscores});
 var i = 0;
 var score = 0;
 var HighScores = document.getElementById("TotalScores");
 var TotalScores = [];
-let questions = [
+var timer = document.querySelector("#timer");
+var timeLeft = 60;
+let questionArray = [
   "What Coding Language controls the functionality of a webpage?",
   "Javascript takes a webpage from Static to  _______?",
   "_______ are commonly used in JavaScript files.",
@@ -15,36 +24,37 @@ let answerChoices = [
   ["Dynamic", "Amazing", "Dramatic"],
   ["Closing Tags", "Headers", "Functions"],
 ];
-let answerGroups = ["1st", "2nd", "3rd"];
-var timer = document.querySelector("#timer");
-var timeLeft = 60;
+let answers = ["2nd", "1st", "3rd"];
 
 
-function StartQuiz(answerVal) {
+
+function Quiz(answerValue) {
+  document.getElementById("instructions").setAttribute("hidden", "hidden");
   document.getElementById("startbtn").setAttribute("hidden", "hidden");
   document.getElementById("score").innerHTML = "Score: ";
   TotalScores.innerHTML = "";
-  startTimer();
+  
 
   if (i > 0 && i <= 3) {
-    if (answerVal === answerGroups[i - 1]) {
+    if (answerValue == answers[i - 1]) {
       score += 10;
-
-    } else if (answerVal == false) {
-       score -= 10;
+      alert("Correct!");
+    } else  {
+      score -= 10;
       timeLeft -= 10;
+      alert("Wrong! -10 points");
     }
   }
-
   if (i == 0) {
     //display answerGroups
     document.getElementById("1st").removeAttribute("hidden");
     document.getElementById("2nd").removeAttribute("hidden");
     document.getElementById("3rd").removeAttribute("hidden");
-
+    document.getElementById("score").removeAttribute("hidden");
     score = 0;
 
-    if (timeLeft < 1 || i == 3) {
+    timerInterval= setInterval(function() {
+    if (timeLeft < 1 || i == 4) {
       clearInterval(timerInterval);
       document.getElementById("timer").innerHTML = "";
       document.getElementById("1st").innerHTML = "";
@@ -66,24 +76,27 @@ function StartQuiz(answerVal) {
 
       timeLeft = 60;
     }
+  },1000)
   }
 
   document.getElementById("score").innerHTML = score;
 
   if (i <= 2) {
-    document.getElementById("1st").innerHTML = answerChoices[i][1];
-    document.getElementById("2nd").innerHTML = answerChoices[i][0];
+    document.getElementById("1st").innerHTML = answerChoices[i][0];
+    document.getElementById("2nd").innerHTML = answerChoices[i][1];
     document.getElementById("3rd").innerHTML = answerChoices[i][2];
-    document.getElementById("question").innerHTML = questions[i];
+    document.getElementById("question").innerHTML = questionArray[i];
   }
 
-  if (i > 2) {
+  if (i == 0){
+    startTimer()
   }
 
   if (i <= 3) {
     i++
   }
 }
+
 
 function startTimer() {
   //timer
@@ -95,38 +108,42 @@ function startTimer() {
       clearInterval(timerInterval);
       document.getElementById("timer").innerHTML = "Times up!";
     }
-  }, 1000);
+  },1000);
 }
 
-function viewHighscores() {
-  // scoreTotal
+function viewHighscores() { 
+  // high scores
+
   HighScores.innerHTML = "";
-  TotalScores.sort(function (a, b) {return b[1] - a[1];});
-
-  for (var i = 0; i < TotalScores.length; i++) {
-    HighScores.innerHTML +=
-      "initials: " + TotalScores[i][0] +
-      " Score: " +  TotalScores[i][1] + "";
-  }
-}
-
-document.getElementById("TotalScores")
-  .addEventListener("click", viewHighscores);
-
-document.querySelectorAll(".answerBtn").forEach((item) => {
-  item.addEventListener("click", function (event) {
-    event.preventDefault();
-    document.getElementById("1st").innerHTML = choices[i][1];
-    console.log(event.target.value);
-
-    var answerVal = event.target.value;
-    if (answerVal === answerGroups[0]) {
-      score += 10;
-        alert("right answer");
-    } else {
-      timeLeft -= 10;
-      score -= 10;
-    }
-    i++;
+  TotalScores.sort(function (a, b) {
+    return b[1] - a[1];
   });
-});
+  if(TotalScores.length > 0) {
+  for (var i = 0; i< TotalScores.length; i++)  {
+    HighScores.innerHTML += "High Scores: " + TotalScores[i][0] + " " + TotalScores[i][1] +
+      " | ";
+  }}
+}
+document.getElementById("TotalScores")
+
+
+  // document.querySelectorAll(".answerBtn").forEach((item) => {
+  // item.addEventListener("click", function (event) {
+  //   event.preventDefault();
+  //   document.getElementById("1st").innerHTML = answerChoices[i][0];
+  //   document.getElementById("2nd").innerHTML = answerChoices[i][1];
+  //   document.getElementById("1st").innerHTML = answerChoices[i][2];
+  //   document.getElementById("question").innerHTML = questionArray[i];
+  //   console.log(event.target.value);
+
+  //  const answerBtn = event.target.value;
+  // if (answerBtn === answers[0]) {
+  //  score += 10;
+  //  alert("right answer");
+  // } else {
+  // timeLeft -= 10;
+  //  score -= 10;
+  // }
+  //   i++;
+  //   });
+  // });
